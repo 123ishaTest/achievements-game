c3854ec56dad8423c43db24988f5c4483835ac34
Isha <ishadijcks@gmail.com>
2021-12-07 22:06:36 +0100

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:123ishaTest/achievement-game.git main:gh-pages
+git push -f git@github.com:123ishaTest/achievements-game.git main:gh-pages
 
 cd -
